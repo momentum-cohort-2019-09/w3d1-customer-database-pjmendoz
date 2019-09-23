2d7a25383815fe0a2d46648a252ef158467bfc0c
pjmendoz <pearl.j.mendoza@gmail.com>
2019-09-23 15:11:44 -0400

    fixing DOB and customer since

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const capitalize = str => str[0].toUpperCase() + str.slice(1)
 
+// function capitalizeA (string) {
+//     let words = string.split(" ")
+//     let array = words.map(word => words[0].toUpperCase() + word.slice(1)).join(" ")
+//     return array
+// }
+
 function customersToHtml (customer) {
     return `
     <div class = "customers">
@@ -7,12 +13,12 @@ function customersToHtml (customer) {
         <div class= "customer_name">${capitalize(customer.name.first)} ${capitalize(customer.name.last)}</div>  
         <div class="customer_email">${customer.email}</div>
         <ul class="locations">
-            <li><span class="customer_location">${customer.location.street}</span></li>
+            <li><span class="customer_location">${(customer.location.street)}</span></li>
             <li><span class="customer_location">${customer.location.city} ${nameToAbbr(customer.location.state)} ${customer.location.postcode}</span>
         </ul>  
         <ul class="dates">
-            <li><div class="customer_dob"><p>DOB:</p>${moment(customer.dob).format("MMM Do YYYY")}</div></li>
-            <li><div class="customer_since"><p>Customer Since:</p>${moment(customer.registered).format("MMM Do YYYY")}</div></li>  
+            <li><div class="customer_dob"> DOB: ${moment(customer.dob).format("MMM Do YYYY")}</div></li>
+            <li><div class="customer_since">Customer Since: ${moment(customer.registered).format("MMM Do YYYY")}</div></li>  
         </ul>
     </div>    
     `
