ce27bc0c76b0c730bbca84cd9d8660115493e4e7
pjmendoz <pearl.j.mendoza@gmail.com>
2019-09-23 14:31:34 -0400

    updating stage area

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const capitalize = str => str[0].toUpperCase() + str.slice(1)
 function customersToHtml (customer) {
     return `
     <div class = "customers">
-        <img src= ${customer.picture.medium}>
-        <div class= "customer_name">${customer.name.first} ${customer.name.last}</div>  
+        <img src= ${customer.picture.large}>
+        <div class= "customer_name">${capitalize(customer.name.first)} ${capitalize(customer.name.last)}</div>  
         <div class="customer_email">${customer.email}</div>
-        <div class="locations">
-            <span class="customer_location">${customer.location.street}</span>
-            <span class="customer_location">${customer.location.city}</span>
-            <span class="customer_location">${customer.location.state}</span>
-            <span class="customer_location">${customer.location.postcode}</span>
-
-        </div>    
+        <ul class="locations">
+            <li><span class="customer_location">${customer.location.street}</span></li>
+            <li><span class="customer_location">${customer.location.city} ${nameToAbbr(customer.location.state)} ${customer.location.postcode}</span>
+        </ul>  
+        <ul class="dates">
+            <li><div class="customer_dob"><p>DOB:</p>${moment(customer.dob).format("MMM Do YYYY")}</div></li>
+            <li><div class="customer_since"><p>Customer Since:</p>${moment(customer.registered).format("MMM Do YYYY")}</div></li>  
+        </ul>
     </div>    
     `
 }
